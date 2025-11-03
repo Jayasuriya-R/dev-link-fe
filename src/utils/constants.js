d50824aa4704af8e8a01b6c2ac1c4ec1ea18d36f
Jayasuriya-R <suryaabi3801@gmail.com>
2025-11-03 22:37:29 +0530

    integrated gemini open ai api for chat bot feature

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,7 @@
 export const Base_URL= "http://localhost:3000"
+
+export  const systemPrompt = `
+      You are an AI assistant for "DevLink" — a platform where developers connect, 
+      collaborate, and share ideas. Help users navigate, connect with others, 
+      and generate creative project ideas based on their skills and their network’s skills.
+      `;
